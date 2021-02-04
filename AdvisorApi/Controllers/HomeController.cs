7f67ba776675b5ff5466f20ac31ca07a77056398
Markus Madeleyn <Markus.Madeleyn@xpertek.co.za>
2021-02-04 09:10:56 +0200

    Add project files.

---
+++ AdvisorApi/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AdvisorApi.Models;
+using CommonLibrary;
+
+namespace AdvisorApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public IActionResult Index(mod incomingMod)
+        {
+            if(incomingMod.msge == null)
+            {
+                mod obj = new mod();
+                obj.msge = Xpert.Advice();
+                return View(obj);
+            }
+            else
+            {
+                return View(incomingMod);
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+    public class mod
+    {
+        public string msge { get; set; }
+    }
+}
